c3991209840de5e9a38357c5aa2136b5964da867
Fatma Ali <alifatma2019@gmail.com>
2020-11-04 15:24:42 +0300

    Update segment.ts (#91)

--- src/helpers/segment.ts
+++ src/helpers/segment.ts
@@ -1,6 +1,6 @@
 import segment from './analytics'
 
-const env_check = process.env.NODE_ENV === 'PROD'
+const env_check = process.env.NODE_ENV === 'production'
 
 const analytics = {
   identify: (id: any) => {
