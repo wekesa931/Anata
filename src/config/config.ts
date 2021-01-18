0a4e888bc751aeb051aca7c0b26354e5d4f883d8
Fatma Ali <alifatma2019@gmail.com>
2021-01-18 09:06:28 +0300

    updated sandbox iframes (#128)

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,17 @@
 import TestConfig from './config.test.json'
 import ProdConfig from './config.prod.json'
+import SandboxConfig from './config.sandbox.json'
 
-const config: any = process.env.PROD ? ProdConfig : TestConfig
+const getConfig = () => {
+  if (process.env.PROD) {
+    return ProdConfig
+  }
+  if (process.env.SANDBOX) {
+    return SandboxConfig
+  }
+  return TestConfig
+}
+
+const config: any = getConfig()
 
 export default config
