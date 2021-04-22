07bde8e4f900c9a4169ab7b765f67a75741c2541
Fatma Ali <alifatma2019@gmail.com>
2021-04-22 21:57:30 +0300

    reduce commmit hash chars, add timezone to release date env

--- src/components/footer/footer.component.tsx
+++ src/components/footer/footer.component.tsx
@@ -3,13 +3,12 @@ import React from 'react'
 import styles from './footer.component.css'
 
 const Footer = () => {
-  console.log(process.env, 'Env Variables')
   return (
     <div className={styles.container}>
       <p>
         Build:{' '}
         {dayjs(process.env.RELEASE_DATE).local().format('DD MMM YYYY hh:mmA')}{' '}
-        {process.env.VERSION}@{process.env.COMMIT}
+        {process.env.VERSION}@{process.env.COMMIT?.substr(0, 6)}
       </p>
       <p>&copy; Antara Health {new Date().getFullYear()}</p>
     </div>
