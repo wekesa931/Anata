f08826060654fde6ff432aac68137e9516e1066f
Farhan Abdirashid <farhanisto@gmail.com>
2021-06-07 07:28:16 +0300

    add missing field in gql query (#172)

--- src/gql/interactions.ts
+++ src/gql/interactions.ts
@@ -46,6 +46,7 @@ const GET_ALL_FLAGGED_INTERACTIONS = gql`
             firstName
             surname
             middleName
+            atRecordId
           }
           healthNavigator {
             fullName
