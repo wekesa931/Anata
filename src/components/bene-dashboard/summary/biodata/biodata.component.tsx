wekesa931/Anata

commit a32cf97413a675bfe0c8c04c5eb492fc77174799

Pr1012 Sidebar redesign (#174)
Implement a new sidebar fresh look

Co-authored-by: Julius Kabangi <kabangijulius@gmail.com>

src/components/bene-dashboard/summary/biodata/biodata.component.tsx
@@ -1,5 +1,6 @@
-import { useParams } from 'react-router-dom'
+import { useParams, useHistory } from 'react-router-dom'
 import React, { useEffect, useState } from 'react'
+import { ArrowLeft } from 'react-feather'
 import { hmp } from '../../../../types/user'
 import styles from './biodata.component.css'
 import airtableFetch from '../../../../resources/airtable-fetch'
@@ -347,10 +348,16 @@ const GeneralSummary = ({ member }: any) => {
 const BioData = () => {
   const { member } = useMember()
   const isMinor = (age: number) => age < 18
+  const history = useHistory()
 
   return (
     <div className={styles.wrapper}>
-      <h2>Summary</h2>
+      <div className={styles.container}>
+        <div className={styles.arrow}>
+          <ArrowLeft onClick={() => history.goBack()} />
+        </div>
+        <h2>Member dashboard</h2>
+      </div>
       {member && (
         <div className={styles.bioDataCard}>
           <div className={styles.beneNameContainer}>

src/components/main-dashboard/main-dashboard.tsx
@@ -1,16 +1,14 @@
 import React from 'react'
-import { always } from 'kremling'
 import styles from './main-dashboard.component.css'
-import CircleChevronRight from '../../assets/img/icons/ circle-chevron-right.svg'
 import AirtableIframe from '../utils/airtableIframe/airtableIframe.component'
 import { useUser } from '../../context/user-context'
 import config from '../../config/config'
 import Tabs from '../utils/tabs/tabs.component'
-import analytics from '../../helpers/segment'
-import FlagForReview from './flag-for-review/flag-for-review.component'
+import { useSidebar } from '../../context/sidebar-context'
 
 const HNDashboard = () => {
   const user = useUser()
+  const { activeView, activeSubView } = useSidebar()
   const { iframes } = config
 
   const getTasksView = () => {
@@ -20,41 +18,11 @@ const HNDashboard = () => {
     return iframes.default.hntasks
   }
 
-  const getMeetingsView = () => {
-    if (user && iframes[user.email]) {
-      return iframes[user.email].meetings
-    }
-    return iframes.default.meetings
-  }
-
-  const views = [
-    {
-      name: 'Members',
-      description: 'View all member details.',
-      airtableUrl: `https://airtable.com/embed/${iframes.default.members}?viewControls=on`,
-    },
-    {
-      name: 'HN Tasks',
-      description: 'Your view of HN tasks',
-      hasCalendar: true,
-    },
-    {
-      name: 'HN Meetings',
-      description: 'Your view of HN meetings',
-      airtableUrl: `https://airtable.com/embed/${getMeetingsView()}?viewControls=on`,
-    },
-    {
-      name: 'Interactions',
-      description: 'All benes interactions in one place',
-      component: () => <FlagForReview />,
-    },
-  ]
-
-  const [activeView, setActiveView] = React.useState(0)
-
-  const AirtableView = ({ view }: { view: number }) => {
-    return !views[view].hasCalendar ? (
-      <AirtableIframe src={views[view].airtableUrl || ''} />
+  const AirtableView = () => {
+    const subListUrl = process.env.PROD ? activeSubView.rootUrl : activeSubView.url_sandbox
+    const listUrl = process.env.PROD ? activeView.rootUrl : activeView.url_sandbox
+    return !activeView.hasCalendar ? (
+      <AirtableIframe src={activeSubView ? subListUrl : listUrl} />
     ) : (
       <Tabs>
         <div label="Calendar">
@@ -76,64 +44,19 @@ const HNDashboard = () => {
   }
 
   React.useEffect(() => {
-    document.title = `Scribe Home: ${views[activeView].name}`
-  }, [activeView, views])
+    document.title = `Scribe Home: ${activeView.name}`
+  }, [activeView])
 
   return (
     <div className={styles.dashboard}>
-      <div className={styles.dashboardLinks}>
-        <h5 className={styles.heading}>All table links</h5>
-        <p className="text-heading-2">Choose a view</p>
-        <div className="margin-top-24">
-          {views.map((view, index) => {
-            return (
-              <button
-                data-testid={view.name}
-                className="btn-unstyled btn-list"
-                onClick={() => {
-                  analytics.page({
-                    title: `Main Dashboard: ${view.name}`,
-                  })
-                  setActiveView(index)
-                }}
-                key={view.name}
-              >
-                <div
-                  className={always('list-item-heading').maybe(
-                    'list-item-active',
-                    index === activeView
-                  )}
-                >
-                  {view.name}
-                  <p>
-                    <CircleChevronRight
-                      fill={
-                        index === activeView
-                          ? 'var(--orange-base)'
-                          : 'var(--blue-base)'
-                      }
-                    />
-                  </p>
-                </div>
-                <p className="list-item-description">{view.description}</p>
-                <div className="divider" />
-              </button>
-            )
-          })}
-        </div>
-      </div>
       <div className={styles.dashboardView}>
         <p
           data-testid="hn-text-heading"
           className="text-heading-2 margin-bottom-16"
         >
-          {views[activeView].name}
+          {activeSubView ? activeSubView.name : activeView.name}
         </p>
-        {views[activeView].component ? (
-          views[activeView].component()
-        ) : (
-          <AirtableView view={activeView} />
-        )}
+        {activeView.component || activeSubView.component ? (activeView.component || activeSubView.component) : (<AirtableView/>)}
       </div>
     </div>
   )

src/components/sidebar/sidebar.component.css
@@ -1,72 +1,56 @@
 .sidebar {
   display: flex;
   flex-direction: column;
-  width: 7rem;
-  background-color: var(--nav-bg);
+  width: 237px;
+  padding: 20px 0 348px;
+  background-color: var(--white);
+  font-size: 16px;
 }
 
 .active .menuLink {
-  border-left: 4px solid var(--orange-base);
-}
-
-.active svg {
-  fill: var(--blue-dark);
-}
-
-.menuLink:hover > h5 {
   color: var(--orange-base);
 }
-
-.menuItem {
-  height: 9rem;
-  text-align: center;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
+.menuLink > h5{
+  font-weight: 100;
+  font-size: 16px;
+  color: #8b95a5;
+}
+.menuItem > span{
+  margin-right: 8px;
 }
 
-.menuLink {
-  padding: 1rem;
-  flex: 2;
-  justify-content: center;
-  align-items: center;
+.menuItem:hover {
+  background-color: var(--greyscale-7);
   cursor: pointer;
-  width: 100%;
+}
+
+
+.menuLink {
+  align-items: center;
+  margin-left: 32px;
+  padding: 8px;
 }
 
 .menuItem.active {
-  background-color: var(--white);
-}
-
-.borderItemTop {
-  width: 100%;
-  background-color: var(--nav-bg);
-  border-radius: 0 0 24px;
-}
-
-.borderItemBottom {
-  width: 100%;
-  background-color: var(--nav-bg);
-  border-radius: 0 24px 0 0;
-}
-
-.borderItemTop,
-.borderItemBottom {
-  width: 100%;
-  background-color: var(--nav-bg);
-  flex: 1;
+  background-color: var(--orange-light);
 }
 
 .logoContainer {
   text-align: center;
-  padding: 1.75rem 2.25rem 0.875rem 1.75rem;
+  border-bottom: 1px solid var(--greyscale-7);
+  padding: 0 16px 0 32px;
+  min-height: 44px;
 }
 
-.logo {
-  padding: 3rem;
+.list {
+  display: flex;
 }
 
-a {
-  width: 100%;
+.subList {
+  display:flex;
+  padding-left: 10px
+}
+
+.arrowRight, .arrowDown {
+  margin-left: -25px;
 }

src/components/sidebar/sidebar.component.tsx
@@ -1,55 +1,140 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { always, toggle } from 'kremling'
-import { Link, useRouteMatch } from 'react-router-dom'
+import { Link, useRouteMatch, useHistory } from 'react-router-dom'
+import { ArrowRight, ArrowDown } from 'react-feather'
 import styles from './sidebar.component.css'
 import FlatLogo from '../../assets/img/logo/Antara Logo@1x.png'
 import SidebarMenuItems from './sidebar.menu'
+import { useUser } from '../../context/user-context'
+import { useSidebar } from '../../context/sidebar-context'
+import analytics from '../../helpers/segment'
+import config from '../../config/config'
 
 const Sidebar = () => {
+  const history = useHistory()
+  const user = useUser()
+  const { iframes } = config
+  const { handleOnClick, activeView, handleSublistClick } = useSidebar()
   const { path } = useRouteMatch()
-  const isActive = (rootUrl: string) =>
-    path === rootUrl || path.startsWith(rootUrl)
+  const [showDropDownTodo, setDropDownTodo] = useState(false)
+  const [showDropDownPopulation, setDropDownPopulation] = useState(false)
+  const [subItem, setSublist] = useState('')
+
+  const getMeetingsView = () => {
+    if (user && iframes[user.email]) {
+      return iframes[user.email].meetings
+    }
+    return iframes.default.meetings
+  }
+  let items: any = []
+  if (path === '/') {
+    items = SidebarMenuItems.slice(0, 8)
+    items[3].rootUrl = `https://airtable.com/embed/${getMeetingsView()}?viewControls=on`
+  } else if (path.includes('member')) {
+    items = SidebarMenuItems
+  }
+  const handleDropDownMenu = e => {
+    if (e.innerText === 'Tasks') {
+      setDropDownTodo(!showDropDownTodo)
+    }
+    if (e.innerText === 'Population') {
+      setDropDownPopulation(!showDropDownPopulation)
+    }
+  }
+  const subList = (item, index) => (
+    <li
+      className={`${styles.subList} ${styles.menuLink}`}
+      onClick={(event) => {
+        analytics.page({
+          title: `Main Dashboard: ${item.name}`,
+        })
+        handleSublistClick(index)
+        setSublist(item.name)
+        path.includes('member') && history.push('/')
+      }}
+      onKeyDown={() => handleOnClick(index)}
+      role="button"
+      tabIndex={index}
+      key={item.name}
+    >
+      <span className={toggle('text-orange', 'text-grey', item.name === subItem)}>{item.icon}</span>
+      <h5 className={toggle('text-orange', 'text-grey', item.name === subItem)}>
+        {item.name}
+      </h5>
+    </li>
+  )
+
+  const lists = (item, index) => (
+    <li
+      className={`${styles.list} ${styles.menuLink}`}
+      onClick={(event) => {
+        analytics.page({
+          title: `Main Dashboard: ${item.name}`,
+        })
+        setDropDownTodo(false)
+        setDropDownPopulation(false)
+        handleOnClick(index)
+        handleDropDownMenu(event.target)
+        path.includes('member') && history.push('/')
+      }}
+      onKeyDown={() => handleOnClick(index)}
+      role="button"
+      tabIndex={index}
+      key={item.name}
+      data-testid={`item-${item.name}`}
+    >
+      {item.subItems && !showDropDownTodo && item.name === 'Tasks' && (
+        <ArrowRight className= {styles.arrowRight} />)}
+      {item.subItems && showDropDownTodo && item.name === 'Tasks' && (
+        <ArrowDown className= {styles.arrowRight} />)}
+      {item.subItems && !showDropDownPopulation && item.name === 'Population' && (
+          <ArrowRight className= {styles.arrowRight} />)}
+      {item.subItems && showDropDownPopulation && item.name === 'Population' && (
+          <ArrowDown className= {styles.arrowDown} />)}
+      <span className={toggle('text-orange', 'text-grey', activeView.name === item.name)}>{item.icon}</span>
+      <h5
+        className={toggle(
+          'text-orange',
+          'text-grey',
+          activeView.name === item.name
+        )}
+      >
+        {item.name === 'Tasks' ? <b>{item.name}</b> : item.name}
+      </h5>
+    </li>
+  )
   return (
     <div className={styles.sidebar}>
       <div className={styles.logoContainer}>
         <Link to="/">
           <img
             src={FlatLogo}
-            width="40px"
-            height="40px"
+            width="32px"
+            height="32px"
             alt="antara small logo"
           />
         </Link>
       </div>
-      {SidebarMenuItems.map((item) => (
-        <div key={item.name} data-testid="sidebar-links">
-          <div
-            className={always(styles.menuItem).maybe(
-              styles.active,
-              isActive(item.rootUrl)
-            )}
-          >
-            {isActive(item.rootUrl) && <div className={styles.borderItemTop} />}
-            <Link to={item.rootUrl}>
-              <button className={`btn-unstyled ${styles.menuLink}`}>
-                <div style={{ marginBottom: '.5rem' }}>{item.icon}</div>
-                <h5
-                  className={toggle(
-                    'text-blue-light',
-                    'text-blue-dark',
-                    !isActive(item.rootUrl)
-                  )}
-                >
-                  {item.name}
-                </h5>
-              </button>
-            </Link>
-            {isActive(item.rootUrl) && (
-              <div className={styles.borderItemBottom} />
-            )}
-          </div>
-        </div>
-      ))}
+      <div className="sidebar" data-testid="sidebar-links">
+        {items.map((item: any, index: any) => {
+          const { subItems } = item
+          return (
+            <div
+              className={always(styles.menuItem).maybe(
+                styles.active,
+                activeView.name === item.name
+              )}
+              key={item.name}
+            >
+              <div className= {item.name}>
+                {lists(item, index)}
+                {subItems && showDropDownTodo && item.name === 'Tasks' && subItems.map( (element,index ) => subList(element, index))}
+                {subItems && showDropDownPopulation && item.name === 'Population' && subItems.map( (element,index ) => subList(element, index))}
+              </div>
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }

commit d4afe27d850abb0450bdc84672c25be702fac765

Fix linting issues that are causing CI to fail introduced by #174 (#178)
* Fix linting issues that are causing CI to fail

* Fix linting issue

.github/workflows/ci.yml
@@ -26,7 +26,7 @@ jobs:
           NODE_AUTH_TOKEN: ${{ secrets.NPM_AUTH_TOKEN }}
         run: npm ci
       - name: Run tests
-        run: npm run test
+        run: npm run lint && npm run format && npm run test
         env:
           NPM_TOKEN: ${{ secrets.NPM_AUTH_TOKEN }}
           SEGMENT_WRITE_KEY: 'dummy_write_key'

src/components/bene-dashboard/summary/biodata/biodata.component.tsx
@@ -148,15 +148,26 @@ const ConditionsSummary = () => {
       </div>
       <div className={`text-small ${styles.conditionsContent}`}>
         {conditions.map((condition: any, index: number) => (
-          <li key={index} style= {{borderBottom:'0.5px lightgrey solid', paddingBottom: '2px', marginBottom: '2px'}}>
+          <li
+            key={index}
+            style={{
+              borderBottom: '0.5px lightgrey solid',
+              paddingBottom: '2px',
+              marginBottom: '2px',
+            }}
+          >
             {index + 1}.{' '}
             {condition.Condition === 'Other'
               ? condition['Other, specify']
               : `${condition.Condition}, `}
-            {condition['Starting clinical status']
-             && `${<b>Starting Clinical Status:</b>}${condition['Starting clinical status']}, `}
-            {condition['Current clinical status']
-              && `${<b>Current Clinical Status:</b>}${condition['Current clinical status']}, `} 
+            {condition['Starting clinical status'] &&
+              `${(<b>Starting Clinical Status:</b>)}${
+                condition['Starting clinical status']
+              }, `}
+            {condition['Current clinical status'] &&
+              `${(<b>Current Clinical Status:</b>)}${
+                condition['Current clinical status']
+              }, `}
             <b>Condition Status:</b> {condition['Condition Status']}
           </li>
         ))}

src/components/main-dashboard/main-dashboard.tsx
@@ -19,8 +19,12 @@ const HNDashboard = () => {
   }
 
   const AirtableView = () => {
-    const subListUrl = process.env.PROD ? activeSubView.rootUrl : activeSubView.url_sandbox
-    const listUrl = process.env.PROD ? activeView.rootUrl : activeView.url_sandbox
+    const subListUrl = process.env.PROD
+      ? activeSubView.rootUrl
+      : activeSubView.url_sandbox
+    const listUrl = process.env.PROD
+      ? activeView.rootUrl
+      : activeView.url_sandbox
     return !activeView.hasCalendar ? (
       <AirtableIframe src={activeSubView ? subListUrl : listUrl} />
     ) : (
@@ -56,7 +60,11 @@ const HNDashboard = () => {
         >
           {activeSubView ? activeSubView.name : activeView.name}
         </p>
-        {activeView.component || activeSubView.component ? (activeView.component || activeSubView.component) : (<AirtableView/>)}
+        {activeView.component || activeSubView.component ? (
+          activeView.component || activeSubView.component
+        ) : (
+          <AirtableView />
+        )}
       </div>
     </div>
   )

src/components/sidebar/sidebar.component.css
@@ -43,7 +43,6 @@
 
 .list {
   display: flex;
-  height: 37px;
 }
 
 .subList {

src/components/sidebar/sidebar.component.tsx
@@ -33,7 +33,7 @@ const Sidebar = () => {
   } else if (path.includes('member')) {
     items = SidebarMenuItems
   }
-  const handleDropDownMenu = e => {
+  const handleDropDownMenu = (e: EventTarget) => {
     if (e.innerText === 'Tasks') {
       setDropDownTodo(!showDropDownTodo)
     }
@@ -41,30 +41,61 @@ const Sidebar = () => {
       setDropDownPopulation(!showDropDownPopulation)
     }
   }
-  const subList = (item, index) => (
+  const subList = (
+    item: {
+      name: string
+      icon:
+        | boolean
+        | React.ReactChild
+        | React.ReactFragment
+        | React.ReactPortal
+        | null
+        | undefined
+    },
+    index: number
+  ) => (
     <li
       className={`${styles.subList} ${styles.menuLink}`}
-      onClick={(event) => {
+      onClick={() => {
         analytics.page({
           title: `Main Dashboard: ${item.name}`,
         })
         handleSublistClick(index)
         setSublist(item.name)
+        // eslint-disable-next-line no-unused-expressions
         path.includes('member') && history.push('/')
       }}
       onKeyDown={() => handleOnClick(index)}
+      // eslint-disable-next-line jsx-a11y/no-noninteractive-element-to-interactive-role
       role="button"
       tabIndex={index}
       key={item.name}
     >
-      <span className={toggle('text-orange', 'text-grey', item.name === subItem)}>{item.icon}</span>
+      <span
+        className={toggle('text-orange', 'text-grey', item.name === subItem)}
+      >
+        {item.icon}
+      </span>
       <h5 className={toggle('text-orange', 'text-grey', item.name === subItem)}>
         {item.name}
       </h5>
     </li>
   )
 
-  const lists = (item, index) => (
+  const lists = (
+    item: {
+      name: string
+      subItems: any
+      icon:
+        | boolean
+        | React.ReactChild
+        | React.ReactFragment
+        | React.ReactPortal
+        | null
+        | undefined
+    },
+    index: number
+  ) => (
     <li
       className={`${styles.list} ${styles.menuLink}`}
       onClick={(event) => {
@@ -75,23 +106,41 @@ const Sidebar = () => {
         setDropDownPopulation(false)
         handleOnClick(index)
         handleDropDownMenu(event.target)
+        // eslint-disable-next-line no-unused-expressions
         path.includes('member') && history.push('/')
       }}
       onKeyDown={() => handleOnClick(index)}
+      // eslint-disable-next-line jsx-a11y/no-noninteractive-element-to-interactive-role
       role="button"
       tabIndex={index}
       key={item.name}
       data-testid={`item-${item.name}`}
     >
       {item.subItems && !showDropDownTodo && item.name === 'Tasks' && (
-        <ArrowRight className= {styles.arrowRight} />)}
+        <ArrowRight className={styles.arrowRight} />
+      )}
       {item.subItems && showDropDownTodo && item.name === 'Tasks' && (
-        <ArrowDown className= {styles.arrowRight} />)}
-      {item.subItems && !showDropDownPopulation && item.name === 'Population' && (
-          <ArrowRight className= {styles.arrowRight} />)}
-      {item.subItems && showDropDownPopulation && item.name === 'Population' && (
-          <ArrowDown className= {styles.arrowDown} />)}
-      <span className={toggle('text-orange', 'text-grey', activeView.name === item.name)}>{item.icon}</span>
+        <ArrowDown className={styles.arrowRight} />
+      )}
+      {item.subItems &&
+        !showDropDownPopulation &&
+        item.name === 'Population' && (
+          <ArrowRight className={styles.arrowRight} />
+        )}
+      {item.subItems &&
+        showDropDownPopulation &&
+        item.name === 'Population' && (
+          <ArrowDown className={styles.arrowDown} />
+        )}
+      <span
+        className={toggle(
+          'text-orange',
+          'text-grey',
+          activeView.name === item.name
+        )}
+      >
+        {item.icon}
+      </span>
       <h5
         className={toggle(
           'text-orange',
@@ -126,10 +175,16 @@ const Sidebar = () => {
               )}
               key={item.name}
             >
-              <div className= {item.name}>
+              <div className={item.name}>
                 {lists(item, index)}
-                {subItems && showDropDownTodo && item.name === 'Tasks' && subItems.map( (element,index ) => subList(element, index))}
-                {subItems && showDropDownPopulation && item.name === 'Population' && subItems.map( (element,index ) => subList(element, index))}
+                {subItems &&
+                  showDropDownTodo &&
+                  item.name === 'Tasks' &&
+                  subItems.map((element: any, i: any) => subList(element, i))}
+                {subItems &&
+                  showDropDownPopulation &&
+                  item.name === 'Population' &&
+                  subItems.map((element: any, i: any) => subList(element, i))}
               </div>
             </div>
           )

src/components/sidebar/sidebar.menu.tsx
@@ -21,7 +21,7 @@ const SidebarMenuItems = [
     name: 'Members',
     icon: <User />,
     rootUrl: `https://airtable.com/embed/${iframes.default.members}?viewControls=on`,
-    url_sandbox: 'https://airtable.com/embed/shrZAZDHnRGQy1Fuw'
+    url_sandbox: 'https://airtable.com/embed/shrZAZDHnRGQy1Fuw',
   },
   {
     name: 'Interactions',
@@ -32,9 +32,30 @@ const SidebarMenuItems = [
     name: 'Tasks',
     icon: <CheckCircle />,
     subItems: [
-      { name: 'HN Tasks', icon: <CheckCircle />, rootUrl: 'https://airtable.com/embed/shrlDv3haSmnW2KXm?backgroundColor=green&viewControls=on', url_sandbox: 'https://airtable.com/embed/shrgqHM3zICH0Nzxr?backgroundColor=pink&viewControls=on'},
-      { name: 'Callbacks', icon: <Phone />, rootUrl: 'https://airtable.com/embed/shrWh9lzYTjGX7fDT?backgroundColor=green&viewControls=on',  url_sandbox: 'https://airtable.com/embed/shrYHGNNigfYV0ewB?backgroundColor=pink&viewControls=on'},
-      { name: 'Members Tasks', icon: <CheckCircle />, rootUrl: 'https://airtable.com/embed/shrz0HHYQVgNuca5v/tbljwCFIDT2vG65AH?backgroundColor=green&viewControls=on',  url_sandbox: 'https://airtable.com/embed/shrRQv6wWFAxMJf4A?backgroundColor=pink&viewControls=on'},
+      {
+        name: 'HN Tasks',
+        icon: <CheckCircle />,
+        rootUrl:
+          'https://airtable.com/embed/shrlDv3haSmnW2KXm?backgroundColor=green&viewControls=on',
+        url_sandbox:
+          'https://airtable.com/embed/shrgqHM3zICH0Nzxr?backgroundColor=pink&viewControls=on',
+      },
+      {
+        name: 'Callbacks',
+        icon: <Phone />,
+        rootUrl:
+          'https://airtable.com/embed/shrWh9lzYTjGX7fDT?backgroundColor=green&viewControls=on',
+        url_sandbox:
+          'https://airtable.com/embed/shrYHGNNigfYV0ewB?backgroundColor=pink&viewControls=on',
+      },
+      {
+        name: 'Members Tasks',
+        icon: <CheckCircle />,
+        rootUrl:
+          'https://airtable.com/embed/shrz0HHYQVgNuca5v/tbljwCFIDT2vG65AH?backgroundColor=green&viewControls=on',
+        url_sandbox:
+          'https://airtable.com/embed/shrRQv6wWFAxMJf4A?backgroundColor=pink&viewControls=on',
+      },
     ],
   },
   {
@@ -46,36 +67,57 @@ const SidebarMenuItems = [
   {
     name: 'Prescription',
     icon: <Clipboard />,
-    rootUrl: 'https://airtable.com/embed/shrJJPowmXuMD3lwD?backgroundColor=green&viewControls=on',
-    url_sandbox: 'https://airtable.com/embed/shribeyiP3MeCBRhH?backgroundColor=pink&viewControls=on'
+    rootUrl:
+      'https://airtable.com/embed/shrJJPowmXuMD3lwD?backgroundColor=green&viewControls=on',
+    url_sandbox:
+      'https://airtable.com/embed/shribeyiP3MeCBRhH?backgroundColor=pink&viewControls=on',
   },
   {
     name: 'HMP',
     icon: <List />,
-    rootUrl:'https://airtable.com/embed/shrTQVb2Z78fziamV?backgroundColor=green&viewControls=on',
-    url_sandbox: 'https://airtable.com/embed/shrWNJpP9NTiUKwjV?backgroundColor=pink&viewControls=on'
+    rootUrl:
+      'https://airtable.com/embed/shrTQVb2Z78fziamV?backgroundColor=green&viewControls=on',
+    url_sandbox:
+      'https://airtable.com/embed/shrWNJpP9NTiUKwjV?backgroundColor=pink&viewControls=on',
   },
   {
     name: 'Conditions',
     icon: <Thermometer />,
-    rootUrl: 'https://airtable.com/embed/shr1IJ9aQ9QhE8O9I?backgroundColor=green&viewControls=on',
-    url_sandbox: 'https://airtable.com/embed/shraxVG3xU1zyzymx?backgroundColor=pink&viewControls=on'
+    rootUrl:
+      'https://airtable.com/embed/shr1IJ9aQ9QhE8O9I?backgroundColor=green&viewControls=on',
+    url_sandbox:
+      'https://airtable.com/embed/shraxVG3xU1zyzymx?backgroundColor=pink&viewControls=on',
   },
   {
     name: 'Interventions',
     icon: <Activity />,
-    rootUrl: 'https://airtable.com/embed/shr0y89LFGiiT4oHz?backgroundColor=green&viewControls=on',
-    url_sandbox: 'https://airtable.com/embed/shriAhSRYFESlmIiP?backgroundColor=pink&viewControls=on'
+    rootUrl:
+      'https://airtable.com/embed/shr0y89LFGiiT4oHz?backgroundColor=green&viewControls=on',
+    url_sandbox:
+      'https://airtable.com/embed/shriAhSRYFESlmIiP?backgroundColor=pink&viewControls=on',
   },
   {
     name: 'Population',
     icon: <Users />,
     subItems: [
-      { name: 'Members', icon: <User />, rootUrl: `https://airtable.com/embed/${iframes.default.members}?viewControls=on` },
-      { name: 'NPS', icon: <TrendingUp />, rootUrl: 'https://airtable.com/embed/shrKb4Z74A074oLaF?backgroundColor=green&viewControls=on' },
-      { name: 'Interactions', icon: <Minimize />, component: <FlagForReview /> },
+      {
+        name: 'Members',
+        icon: <User />,
+        rootUrl: `https://airtable.com/embed/${iframes.default.members}?viewControls=on`,
+      },
+      {
+        name: 'NPS',
+        icon: <TrendingUp />,
+        rootUrl:
+          'https://airtable.com/embed/shrKb4Z74A074oLaF?backgroundColor=green&viewControls=on',
+      },
+      {
+        name: 'Interactions',
+        icon: <Minimize />,
+        component: <FlagForReview />,
+      },
     ],
-    url_sandbox: ''
+    url_sandbox: '',
   },
 ]
 

src/context/sidebar-context.tsx
@@ -10,13 +10,13 @@ type sidebar = {
 }
 
 const SidebarContext = React.createContext<sidebar>({
-  handleOnClick: (index: number) => {
+  handleOnClick: () => {
     return null
   },
   activeSubView: '',
   activeIndex: 0,
   activeView: '',
-  handleSublistClick: (index: number) => {
+  handleSublistClick: () => {
     return null
   },
 })
@@ -41,6 +41,7 @@ const SidebarProvider = ({ children }: any) => {
   }, [])
 
   useEffect(() => {
+    // eslint-disable-next-line
     activeView.subItems && setActiveSubViewItem(activeView.subItems[activeSubViewIndex])
   }, [activeSubViewIndex, activeView])
 