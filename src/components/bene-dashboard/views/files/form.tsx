14009d0d0fb8319c92faabc0fec5de25c4774675
shaddyshad <108327655+shadrackmwangi@users.noreply.github.com>
2022-09-08 14:53:53 +0300

    Pr4475/udm docs sharing scribe (#507)
    * feat: add new form fields for sharing docs on UDM
    
    * fix: eslints
    
    * fix: add document sharing experience when uploading
    
    * fix: add analytics events to shared docs
    
    * fix: lints
    
    * fix: use material select control over auto-complete
    
    * fix: linting

--- src/components/bene-dashboard/views/files/form.tsx
+++ src/components/bene-dashboard/views/files/form.tsx
@@ -1,16 +1,25 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Search } from 'react-feather'
 import Box from '@mui/material/Box'
 import TextField from '@mui/material/TextField'
 import Autocomplete from '@mui/material/Autocomplete'
 import Button from '@mui/material/Button'
+import { Checkbox, FormControlLabel, InputLabel } from '@mui/material'
+import { useQuery } from '@apollo/client'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select, { SelectChangeEvent } from '@mui/material/Select'
 import { useUser } from '../../../../context/user-context'
 import LoadingIcon from '../../../../assets/img/icons/loading.svg'
+import { GET_FOLDERS } from './files.gql'
+import { useMember } from '../../../../context/member.context'
 
 type DocMeta = {
   docType: string
   description: string
   title: string
+  shareWith?: string[]
+  folder?: string
 }
 
 type IProps = {
@@ -18,11 +27,49 @@ type IProps = {
   uploadDocument: (docMeta: DocMeta) => void
 }
 
+type AppFolders = {
+  id: string
+  name: string
+}
+
 const RefinedFileMetaForm = ({ uploadStart, uploadDocument }: IProps) => {
   const user = useUser()
-  const [docType, setDocType] = useState<string>(null)
+  const { member } = useMember()
+  const [docType, setDocType] = useState<string>('')
   const [description, setDescription] = useState('')
   const [title, setTitle] = useState('')
+  const [shareChecked, setShareChecked] = useState(false)
+  const [shareFolder, setShareFolder] = useState<string>('')
+  const [folders, setFolders] = useState<AppFolders[]>([])
+
+  const { data } = useQuery(GET_FOLDERS)
+
+  useEffect(() => {
+    if (data) {
+      const rawFolders = data?.folders?.edges
+
+      setFolders(rawFolders.map(({ node }) => node))
+    }
+  }, [data])
+
+  const handleUploadDoc = () => {
+    let meta: DocMeta = {
+      docType,
+      description: description || title,
+      title,
+    }
+
+    if (shareChecked && !!shareFolder) {
+      meta = {
+        ...meta,
+        shareWith: [member['Antara ID']],
+        folder: shareFolder,
+      }
+    }
+
+    uploadDocument(meta)
+  }
+
   const options = [
     'HMP',
     'Prescription',
@@ -53,25 +100,32 @@ const RefinedFileMetaForm = ({ uploadStart, uploadDocument }: IProps) => {
     setDescription(e.target.value)
   }
 
+  const renderSearchBox = (
+    label: string,
+    searchOptions: Array<string>,
+    handleChange: (v: string) => void,
+    defaultValue = ''
+  ) => (
+    <Box className="relative">
+      <label htmlFor="combo-box-demo" className="form-label search-input-label">
+        {label}
+      </label>
+      <Search className="input-search" width={15} height={15} />
+      <Autocomplete
+        className="auto-complete-input"
+        disablePortal
+        id="combo-box-demo"
+        options={searchOptions}
+        defaultValue={defaultValue}
+        onChange={(_e, newValue) => handleChange(newValue)}
+        renderInput={(params) => <TextField {...params} />}
+      />
+    </Box>
+  )
+
   return (
     <>
-      <Box className="relative">
-        <label
-          htmlFor="combo-box-demo"
-          className="form-label search-input-label"
-        >
-          Search Document Type...
-        </label>
-        <Search className="input-search" width={15} height={15} />
-        <Autocomplete
-          className="auto-complete-input"
-          disablePortal
-          id="combo-box-demo"
-          options={options}
-          onChange={(_e, newValue) => setDocType(newValue)}
-          renderInput={(params) => <TextField {...params} />}
-        />
-      </Box>
+      {renderSearchBox('Search Document Type...', options, setDocType)}
       <label htmlFor="standard-basic" className="form-label">
         Title*
       </label>
@@ -108,17 +162,45 @@ const RefinedFileMetaForm = ({ uploadStart, uploadDocument }: IProps) => {
           value={user?.email}
         />
       </Box>
+      <label htmlFor="standard-basic" className="form-label">
+        Sharing
+      </label>
+      <Box className="upload-fields">
+        <FormControlLabel
+          control={
+            <Checkbox
+              onChange={(event) => setShareChecked(event.target.checked)}
+            />
+          }
+          label="Share doc with member once it's uploaded"
+          sx={{ mt: 1, color: 'var(--dark-blue-50)', fontSize: '16px' }}
+          checked={shareChecked}
+        />
+      </Box>
+      {shareChecked && (
+        <FormControl fullWidth sx={{ mb: 2, mt: 2 }}>
+          <InputLabel id="folder">Choose folder</InputLabel>
+          <Select
+            labelId="folder"
+            id="select-folder"
+            value={shareFolder}
+            onChange={(e: SelectChangeEvent) =>
+              setShareFolder(e.target.value as string)
+            }
+          >
+            {folders.map((f) => (
+              <MenuItem key={f.id} value={f.name}>
+                {f.name}{' '}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      )}
       <Box className="doc-upload-button">
         <Button
           variant={uploadStart ? 'outlined' : 'contained'}
           disabled={!docType || !title || uploadStart}
-          onClick={() =>
-            uploadDocument({
-              docType,
-              description: description || title,
-              title,
-            })
-          }
+          onClick={handleUploadDoc}
         >
           {uploadStart ? (
             <div className="d-flex flex-direction-column flex-align-center">
