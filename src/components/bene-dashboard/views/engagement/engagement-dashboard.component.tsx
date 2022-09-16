edd71e1a921b0f935f61754ecf05273373596e64
Kennedy Njeri <mistakenz123@gmail.com>
2022-09-16 14:08:51 +0300

    engagement view populated with right antara id (#521)
    Co-authored-by: Kennedy <kennedy@antarahealth.com>

--- src/components/bene-dashboard/views/engagement/engagement-dashboard.component.tsx
+++ src/components/bene-dashboard/views/engagement/engagement-dashboard.component.tsx
@@ -5,14 +5,16 @@ import { useMember } from '../../../../context/member.context'
 const EngagementDashboard = () => {
   const { member } = useMember()
 
-  const filters = { 'Antara Id': member['Antara ID'] }
+  const filters = { 'Antara Id': [member['Antara ID'] ]}
+
 
   const options = {
     hideTabs: true,
+    hideToolbar: true
   }
 
   const dashboardURL =
-    'https://dub01.online.tableau.com/t/kenyanairobi/views/Engagement-EmbedScribe/Story1?:showAppBanner=false&:display_count=n&:showVizHome=n&:origin=viz_share_link'
+    'https://dub01.online.tableau.com/t/kenyanairobi/views/Engagement-EmbedScribe/Story1/a6b6fc90-448d-4295-a0c9-9ff2694cd6bd/d76ead17-55a8-4bc0-9162-a38cba09563a?:display_count=n&:showVizHome=n&:origin=viz_share_link'
 
   return (
     <TableauReport url={dashboardURL} filters={filters} options={options} />
