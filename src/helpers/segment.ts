873315fce17aa2c67c431e2f35877b0ee44599a5
Collins Oduor <oduor@antarahealth.com>
2022-09-21 15:40:25 +0300

    revert unintended changes; test that I cannot push to master

--- src/helpers/segment.ts
+++ src/helpers/segment.ts
@@ -4,13 +4,13 @@ const env_check = process.env.NODE_ENV === 'production'
 
 const analytics = {
   identify: (id: any) => {
-    if (env_check) segment?.identify(id) // eslint-disable-line no-unused-expressions
+    if (env_check) segment.identify(id)
   },
   track: (name: any, props?: any) => {
-    if (env_check) segment?.track(name, props) // eslint-disable-line no-unused-expressions
+    if (env_check) segment.track(name, props)
   },
   page: (data?: any, options?: any, callback?: any) => {
-    if (env_check) segment?.page(data, options, callback) // eslint-disable-line no-unused-expressions
+    if (env_check) segment.page(data, options, callback)
   },
 }
 export default analytics
