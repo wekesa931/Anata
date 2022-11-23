7141d3d43948e1ef45512a5d5af342f2d163006e
Kennedy Njeri <mistakenz123@gmail.com>
2022-11-23 13:33:04 +0300

    Pr5089: create engagement score (#684)
    Co-authored-by: Kennedy <kennedy@antarahealth.com>

--- src/components/bene-dashboard/views/engagement/engagement-dashboard.component.tsx
+++ src/components/bene-dashboard/views/engagement/engagement-dashboard.component.tsx
@@ -5,7 +5,7 @@ import { useMember } from '../../../../context/member.context'
 function EngagementDashboard() {
   const { member } = useMember()
 
-  const filters = { 'Antara Id': [member['Antara ID']] }
+  const filters = { 'Antara Id (Members)': [member['Antara ID']] }
 
   const options = {
     hideTabs: true,
@@ -13,7 +13,7 @@ function EngagementDashboard() {
   }
 
   const dashboardURL =
-    'https://dub01.online.tableau.com/t/kenyanairobi/views/Engagement-EmbedScribe/Story1/a6b6fc90-448d-4295-a0c9-9ff2694cd6bd/d76ead17-55a8-4bc0-9162-a38cba09563a?:display_count=n&:showVizHome=n&:origin=viz_share_link'
+    'https://dub01.online.tableau.com/t/kenyanairobi/views/WIPMemberEngagementDashboards/ScribeEmbed-MemberLevelEngagementSummary'
 
   return (
     <TableauReport url={dashboardURL} filters={filters} options={options} />
