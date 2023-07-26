b480e6c3b9cd32e0218fc444e9aa95b3637e3369
Bill Adams Wekesa <wekesabill@gmail.com>
2023-07-26 14:12:30 +0300

    Fix tailwing css (#1097)
    fix tailwing css

--- src/components/feedbacks/member-not-synced.tsx
+++ src/components/feedbacks/member-not-synced.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function MemberNotSyncedToHNOS() {
   return (
-    <div className="flex absolute inset-x-0 top-90 h-16 z-10 w-full text-white bg-red-100 items-center justify-center">
+    <div className="flex absolute inset-x-0 top-3/4 h-16 z-10 w-full text-white bg-red-100 items-center justify-center">
       <p>Member not synced to HNOS. Contact support to fix</p>
     </div>
   )
