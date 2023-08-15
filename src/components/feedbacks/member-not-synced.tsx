a6c52760c1808948936f7a634ccae0ab0d086f59
shaddyshad <108327655+shadrackmwangi@users.noreply.github.com>
2023-08-15 15:08:17 +0300

    fix: position banner at the bottom (#1120)
    Co-authored-by: Intwaza Belyse <97509985+Intwaza-Belyse@users.noreply.github.com>

--- src/components/feedbacks/member-not-synced.tsx
+++ src/components/feedbacks/member-not-synced.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { AlertCircle } from 'react-feather'
 
 function MemberNotSyncedToHNOS() {
   return (
-    <div className="flex absolute inset-x-0 top-3/4 h-16 z-10 w-full text-white bg-red-100 items-center justify-center">
+    <div className="flex font-rubik text-base gap-2 absolute inset-x-0 bottom-0 h-12 z-10 w-full text-white bg-red-100 items-center justify-center">
+        <AlertCircle />
       <p>Member not synced to HNOS. Contact support to fix</p>
     </div>
   )
