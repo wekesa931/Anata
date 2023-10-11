d7b668401623ed35dca3d2bda1ff102e10a76464
Intwaza Belyse <97509985+Intwaza-Belyse@users.noreply.github.com>
2023-10-11 13:20:48 +0200

    add datadog integration (#1137)
    * add datadog integration
    
    * add environment variables for Datadog integration

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from 'src/App'
+import { datadogRum } from '@datadog/browser-rum'
+
+const { DATADOG_APPLICATION_ID, DATADOG_CLIENT_TOKEN, PROD } = process.env
+
+if (!DATADOG_APPLICATION_ID || !DATADOG_CLIENT_TOKEN) {
+  throw new Error(
+    'DATADOG_APPLICATION_ID or DATADOG_CLIENT_TOKEN environment variable is not defined'
+  )
+}
+
+datadogRum.init({
+  applicationId: DATADOG_APPLICATION_ID,
+  clientToken: DATADOG_CLIENT_TOKEN,
+  site: 'us5.datadoghq.com',
+  service: 'scribe',
+  env: PROD,
+  version: '1.0.0',
+  sessionSampleRate: 100,
+  premiumSampleRate: 100,
+  trackUserInteractions: true,
+  defaultPrivacyLevel: 'mask-user-input',
+})
+
+datadogRum.startSessionReplayRecording()
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
