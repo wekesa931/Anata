57b1d14a325245ec5742985286b660c2aee796cf
Kennedy Njeri <mistakenz123@gmail.com>
2023-11-21 17:52:55 +0300

    Feat/overflow auto fix modal (#1306)
    * Feat/overflow auto fix modal
    
    * code review changes
    
    * remove margin right
    
    ---------
    
    Co-authored-by: Kennedy <kennedy@antarahealth.com>

--- src/components/modals/primary.tsx
+++ src/components/modals/primary.tsx
@@ -9,7 +9,7 @@ type PrimaryModalProps = ModalProps & {
 function PrimaryModal(props: PrimaryModalProps) {
   return (
     <Modal {...props}>
-      <Box className="bg-white-100 p-6 rounded-lg absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 overflow-scroll min-w-1/2 max-h-full">
+      <Box className="bg-white-100 p-6 rounded-lg absolute inset-x-1/2 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 overflow-auto min-w-1/2 max-h-full">
         {props.title && (
           <header className="bg-white-100 flex justify-between items-center font-rubik text-xl font-semibold">
             {props.title}
