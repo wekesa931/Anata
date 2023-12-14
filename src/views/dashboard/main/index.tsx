1ca7fdb738a5c8ebcae417a1f0d854235de5b63f
shaddyshad <108327655+shadrackmwangi@users.noreply.github.com>
2023-12-14 16:05:08 +0300

    fix: update z-index for main (#1340)
    * fix: update z-index for main
    
    * fix: formats

--- src/views/dashboard/main/index.tsx
+++ src/views/dashboard/main/index.tsx
@@ -114,7 +114,7 @@ function MainDashboard() {
 
   return (
     <div className="flex h-full font-rubik flex-col w-full overflow-x-hidden relative ">
-      <nav className="flex border-bottom items-center w-full h-14 sticky top-0 bg-dashboard-blue gap-4 py-[2%] px-4 ml-1.5 z-2">
+      <nav className="flex border-bottom items-center w-full h-14 sticky top-0 bg-dashboard-blue gap-4 py-[2%] px-4 ml-1.5 z-20">
         <IconButton
           className="bg-blue-20 rounded-full"
           onClick={() => setCollapsed(!collapsed)}
@@ -127,7 +127,7 @@ function MainDashboard() {
           <SearchInput />
         </div>
       </nav>
-      <div className="flex overflow-x-hidden h-full z-1">
+      <div className="flex overflow-x-hidden h-full z-10">
         <Sidebar
           toggled={toggled}
           onBackdropClick={() => setToggled(false)}
