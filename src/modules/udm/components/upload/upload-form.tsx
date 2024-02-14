b63b20ba3cfee891df36ad0cd155275162bef7b8
shaddyshad <108327655+shadrackmwangi@users.noreply.github.com>
2024-02-14 14:08:09 +0300

    Fix/labs required field (#1387)
    * fix: rename and re-use exiting column
    
    * fix: lower the gaps
    
    * fix: use record ids to pull task templates
    
    * fix: lints
    
    * fix: mark linked labs as optional
    
    * fix: add a validation schema and mark linkedLabRequest as optional
    
    * fix: lints

--- src/modules/udm/components/upload/upload-form.tsx
+++ src/modules/udm/components/upload/upload-form.tsx
@@ -11,6 +11,33 @@ import TextField from 'src/components/forms/fields/text'
 import PrimaryForm from 'src/components/forms/primary-form'
 import { DocMeta } from 'src/modules/udm/types'
 import { useDocumentUpload } from 'src/modules/udm/hooks/document-upload'
+import * as yup from 'yup'
+import { LabRequest } from 'src/modules/labs/types'
+
+const validationSchema = yup.object().shape({
+  docType: yup.string().required(),
+  title: yup.string().required(),
+  creator: yup.string().required(),
+  description: yup.string().optional(),
+  linkedLabRequest: yup
+    .array()
+    .of(
+      yup
+        .mixed<LabRequest>()
+        .typeError('Invalid lab request')
+        .test('is-valid', 'Invalid lab request', (value: any) => {
+          return 'labType' in value && 'imagingType' in value // this is a rudimentary test to check for the type
+        })
+    )
+    .optional(),
+  createReviewTask: yup.boolean().optional(),
+  shareDocument: yup.boolean().optional(),
+  folder: yup.string().when('shareDocument', {
+    is: true,
+    then: yup.string().required(),
+    otherwise: yup.string().optional(),
+  }),
+})
 
 type Props = {
   options: Options[]
@@ -47,136 +74,145 @@ function UploadDocumentForm({
       initialValues={formValues}
       handleSubmit={submitDocument}
       className="font-rubik"
+      validationSchema={validationSchema}
     >
-      {(formik) => (
-        <Form>
-          <GroupedSearchField
-            name="docType"
-            options={options}
-            label="Search document type..."
-            placeholder="Search..."
-            multiple
-            disabled={submittingDocument}
-          />
-
-          {formik.values.docType === LAB_REPORT_DOC_TYPE && (
-            <MultiselectField
-              name="linkedLabRequest"
-              options={labRequests}
-              label="Search lab request..."
-              placeholder="Search..."
-              disabled={submittingDocument}
-            />
-          )}
-
-          <TextField
-            name="title"
-            label="Enter the title"
-            placeholder="Title"
-            disabled={submittingDocument}
-          />
-          <TextField
-            name="description"
-            label="Description"
-            placeholder="Description"
-            textarea
-            required={false}
-            disabled={submittingDocument}
-          />
-          <TextField name="creator" label="Creator" disabled required={false} />
-          <p className="text-dark-blue-50 text-base font-medium">Sharing</p>
-          <div className="mb-4">
-            <FormControlLabel
-              disabled={submittingDocument}
-              control={
-                <Checkbox
-                  checked={formik.values.createReviewTask}
-                  onChange={formik.handleChange}
-                  sx={{
-                    '&.Mui-checked': {
-                      color: 'var(--blue-100)',
-                    },
-                    color: 'var(--dark-blue-50)',
-                  }}
-                />
-              }
-              label={
-                <p className="font-rubik text-base text-dark-blue-70">
-                  Automatically create a document review task for the assigned
-                  HN
-                </p>
-              }
-              name="createReviewTask"
-            />
-          </div>
-          <div className="mb-4">
-            <FormControlLabel
-              disabled={submittingDocument}
-              control={
-                <Checkbox
-                  checked={formik.values.shareDocument}
-                  onChange={formik.handleChange}
-                  sx={{
-                    '&.Mui-checked': {
-                      color: 'var(--blue-100)',
-                    },
-                    color: 'var(--dark-blue-50)',
-                  }}
-                />
-              }
-              label={
-                <p className="font-rubik text-base text-dark-blue-70">
-                  Share doc with member once it&apos;s uploaded
-                </p>
-              }
-              name="shareDocument"
-            />
-          </div>
-          {formik.values.shareDocument && (
+      {(formik) => {
+        return (
+          <Form>
             <GroupedSearchField
-              name="folder"
-              options={folders}
-              label="Choose upload folder"
-              placeholder="Search Folder..."
+              name="docType"
+              options={options}
+              label="Search document type..."
+              placeholder="Search..."
+              multiple
               disabled={submittingDocument}
             />
-          )}
 
-          {showProgressFeedback ? (
-            <div className="w-full flex flex-col gap-2 font-rubik">
-              <div className="w-full flex justify-between items-center text-base text-dark-blue-100">
-                {processTitle}
-                <span>{progress} %</span>
-              </div>
-              <LinearProgress
-                variant="determinate"
-                value={Math.round(progress)}
-                sx={{
-                  '& .MuiLinearProgress-bar': {
-                    backgroundColor: error ? '#972323' : '#007AFF',
-                  },
-                }}
+            {formik.values.docType === LAB_REPORT_DOC_TYPE && (
+              <MultiselectField
+                name="linkedLabRequest"
+                options={labRequests}
+                label="Search lab request..."
+                placeholder="Search..."
+                disabled={submittingDocument}
+                required={false}
               />
-              {error && (
-                <div className="flex flex-col gap-2">
-                  <p className="text-xs text-dark-red-100">{error}</p>
-                  <div className="w-full flex justify-end">
-                    <PrimaryButton onClick={retryCurrentProcess}>
-                      Retry
-                    </PrimaryButton>
-                  </div>
+            )}
+
+            <TextField
+              name="title"
+              label="Enter the title"
+              placeholder="Title"
+              disabled={submittingDocument}
+            />
+            <TextField
+              name="description"
+              label="Description"
+              placeholder="Description"
+              textarea
+              required={false}
+              disabled={submittingDocument}
+            />
+            <TextField
+              name="creator"
+              label="Creator"
+              disabled
+              required={false}
+            />
+            <p className="text-dark-blue-50 text-base font-medium">Sharing</p>
+            <div className="mb-4">
+              <FormControlLabel
+                disabled={submittingDocument}
+                control={
+                  <Checkbox
+                    checked={formik.values.createReviewTask}
+                    onChange={formik.handleChange}
+                    sx={{
+                      '&.Mui-checked': {
+                        color: 'var(--blue-100)',
+                      },
+                      color: 'var(--dark-blue-50)',
+                    }}
+                  />
+                }
+                label={
+                  <p className="font-rubik text-base text-dark-blue-70">
+                    Automatically create a document review task for the assigned
+                    HN
+                  </p>
+                }
+                name="createReviewTask"
+              />
+            </div>
+            <div className="mb-4">
+              <FormControlLabel
+                disabled={submittingDocument}
+                control={
+                  <Checkbox
+                    checked={formik.values.shareDocument}
+                    onChange={formik.handleChange}
+                    sx={{
+                      '&.Mui-checked': {
+                        color: 'var(--blue-100)',
+                      },
+                      color: 'var(--dark-blue-50)',
+                    }}
+                  />
+                }
+                label={
+                  <p className="font-rubik text-base text-dark-blue-70">
+                    Share doc with member once it&apos;s uploaded
+                  </p>
+                }
+                name="shareDocument"
+              />
+            </div>
+            {formik.values.shareDocument && (
+              <GroupedSearchField
+                name="folder"
+                options={folders}
+                label="Choose upload folder"
+                placeholder="Search Folder..."
+                disabled={submittingDocument}
+              />
+            )}
+
+            {showProgressFeedback ? (
+              <div className="w-full flex flex-col gap-2 font-rubik">
+                <div className="w-full flex justify-between items-center text-base text-dark-blue-100">
+                  {processTitle}
+                  <span>{progress} %</span>
                 </div>
-              )}
-            </div>
-          ) : (
-            <div className="w-full flex justify-end mt-2">
-              <PrimaryButton type="submit" disabled={submittingDocument}>
-                Save
-              </PrimaryButton>
-            </div>
-          )}
-        </Form>
-      )}
+                <LinearProgress
+                  variant="determinate"
+                  value={Math.round(progress)}
+                  sx={{
+                    '& .MuiLinearProgress-bar': {
+                      backgroundColor: error ? '#972323' : '#007AFF',
+                    },
+                  }}
+                />
+                {error && (
+                  <div className="flex flex-col gap-2">
+                    <p className="text-xs text-dark-red-100">{error}</p>
+                    <div className="w-full flex justify-end">
+                      <PrimaryButton onClick={retryCurrentProcess}>
+                        Retry
+                      </PrimaryButton>
+                    </div>
+                  </div>
+                )}
+              </div>
+            ) : (
+              <div className="w-full flex justify-end mt-2">
+                <PrimaryButton type="submit" disabled={submittingDocument}>
+                  Save
+                </PrimaryButton>
+              </div>
+            )}
+          </Form>
+        )
+      }}
     </PrimaryForm>
   )
 }
