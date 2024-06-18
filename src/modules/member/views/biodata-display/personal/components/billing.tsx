acad604de9b963117e83cc55917a9ad529eeec7f
Intwaza Belyse <97509985+Intwaza-Belyse@users.noreply.github.com>
2024-06-18 10:07:21 +0200

    fix: minor billing qa changes (#1571)

--- src/modules/member/views/biodata-display/personal/components/billing.tsx
+++ src/modules/member/views/biodata-display/personal/components/billing.tsx
@@ -18,13 +18,7 @@ import { ExpandMoreOutlined } from '@mui/icons-material'
 import DoneIcon from '@mui/icons-material/Done'
 import Tooltip from 'src/components/tooltip'
 import { GridCloseIcon } from '@mui/x-data-grid'
-import { format } from 'date-fns'
-import dayjs from 'dayjs'
 import EmptyDataIcon from 'src/assets/img/icons/empty-data.svg'
-
-function formatDate(dateString: any) {
-  return format(new Date(dateString), 'MMMM yyyy')
-}
 
 function BillingSectionItem({
   memberCohortItem,
@@ -34,21 +28,9 @@ function BillingSectionItem({
   const [expanded, setExpanded] = useState<boolean>(false)
   const memberConsent =
     memberCohortItem?.isOptInRequired === 'Yes' && !!memberCohortItem?.optedInAt
-  const billingStatus =
-    memberCohortItem?.subscriptionStatus === 'ACTIVE'
-      ? 'Eligible'
-      : 'Not eligible'
+
   return (
     <div>
-      {billingStatus === 'Not eligible' && (
-        <div className="flex flex-col h-full">
-          <div className="mb-4">
-            <ItemTitle title="Reason(s)" />
-          </div>
-          {memberCohortItem?.remarks}
-        </div>
-      )}
-
       <Accordion
         expanded={expanded}
         onChange={() => setExpanded(!expanded)}
@@ -117,40 +99,6 @@ function BillingSectionItem({
                 />
               </div>
             </GridItems>
-            <div>
-              <h1 className="text-dark-blue-100 font-semibold">
-                Recent Bills:{' '}
-              </h1>
-            </div>
-
-            {memberCohortItem?.billingEvents?.length === 0 ? (
-              <div className="justify-center items-center flex w-full p-2">
-                <p className="text-dark-blue-50 font-rubik text-center text-xs">
-                  This member does not have any recent bills
-                </p>
-              </div>
-            ) : (
-              <>
-                {memberCohortItem?.billingEvents?.map((events) => (
-                  <div className="flex justify-between items-center">
-                    <div>
-                      <h4 className="text-dark-blue-100 text-center font-rubik text-sm flex justify-start">
-                        {dayjs(events?.createdAt).format('DD MMMM YYYY')}
-                      </h4>
-                      <p className="text-xs text-dark-blue-50 ">
-                        Subscription{' '}
-                        {formatDate(events?.billingPeriodStartDate)} to{' '}
-                        {formatDate(events?.billingPeriodEndDate)}
-                      </p>
-                    </div>
-                    <ItemChild
-                      child={events?.amount}
-                      className="text-dark-blue-50"
-                    />
-                  </div>
-                ))}
-              </>
-            )}
           </>
         </AccordionDetails>
       </Accordion>
@@ -166,6 +114,21 @@ function BillingSection({ member }: BillingSectionProps) {
   const verification =
     member?.verificationStatus === 'VERIFIED' ? 'Eligible' : 'Not eligible'
   const memberCohortDetails: MemberCohortType[] = member?.membercohortSet || []
+
+  const cohortsWithRemarks = memberCohortDetails.filter(
+    (cohort) => cohort.remarks
+  )
+
+  const billingEligibility = () => {
+    const activeCohorts = memberCohortDetails.filter(
+      (cohort) => cohort.subscriptionStatus === 'ACTIVE'
+    )
+    if (activeCohorts.length > 0) {
+      return 'Eligible'
+    }
+
+    return 'Not eligible'
+  }
 
   return member ? (
     <div id="billing-section">
@@ -191,44 +154,37 @@ function BillingSection({ member }: BillingSectionProps) {
             </div>
           </div>
         </GridItems>
+
         <GridItems single>
           <div className="flex items-center justify-between gap-1 mb-2">
             <p className="text-dark-blue-100">Billing Eligibility:</p>
-            {memberCohortDetails.length === 0 ? (
-              <div className="flex items-center gap-2">
-                <ItemChild
-                  child="Not eligible"
-                  className="ml-2 text-dark-blue-50"
-                />
-                <GridCloseIcon className="text-[#ebfbed] bg-rose-500 w-4 h-4 rounded-2xl" />
-              </div>
-            ) : (
-              <>
-                {memberCohortDetails.map((cohort) => {
-                  const billingStatus =
-                    cohort?.subscriptionStatus === 'ACTIVE'
-                      ? 'Eligible'
-                      : 'Not eligible'
-                  return (
-                    <div className="flex items-center gap-2">
-                      <ItemChild
-                        child={billingStatus}
-                        className="ml-2 text-dark-blue-50"
-                      />
-                      <Tooltip>
-                        {billingStatus === 'Not eligible' ? (
-                          <GridCloseIcon className="text-[#ebfbed] bg-rose-500 w-4 h-4 rounded-2xl" />
-                        ) : (
-                          <DoneIcon className="text-[#ebfbed] bg-[#34c759] w-4 h-4 rounded-2xl" />
-                        )}
-                      </Tooltip>
-                    </div>
-                  )
-                })}
-              </>
-            )}
+            <div className="flex items-center gap-2">
+              <ItemChild
+                child={billingEligibility()}
+                className="ml-2 text-dark-blue-50"
+              />
+              <Tooltip>
+                {billingEligibility() === 'Eligible' ? (
+                  <DoneIcon className="text-[#ebfbed] bg-[#34c759] w-4 h-4 rounded-2xl" />
+                ) : (
+                  <GridCloseIcon className="text-[#ebfbed] bg-rose-500 w-4 h-4 rounded-2xl" />
+                )}
+              </Tooltip>
+            </div>
           </div>
         </GridItems>
+
+        {billingEligibility() === 'Not eligible' &&
+          cohortsWithRemarks.length > 0 && (
+            <div className="flex flex-col h-full">
+              <div className="mb-4">
+                <ItemTitle title="Reason(s)" />
+              </div>
+              {memberCohortDetails.map((cohort, index) => (
+                <p key={index}>{cohort.remarks}</p>
+              ))}
+            </div>
+          )}
 
         <div className="mb-4 mt-5">
           <ItemTitle title="Cohorts" />
