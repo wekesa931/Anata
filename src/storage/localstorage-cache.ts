86293b72f4e6b924cd680d06570735b608c6c46a
Clinton <33593967+clintonkimathi@users.noreply.github.com>
2024-09-16 17:21:51 +0300

    Fix: Resolve issue with fetching cached data  (#1669)
    * fix: resolve issue with fetching data from local cache
    
    * fix: code cleanup for readability purpose

--- src/storage/localstorage-cache.ts
+++ src/storage/localstorage-cache.ts
@@ -4,14 +4,22 @@ export enum CACHE_KEYS {
   CONDITION_DEFINITIONS = 'CONDITION_DEFINITIONS',
   CONDITION_LOOKUPS = 'CONDITION_LOOKUPS',
 }
-
+const getCachedData = (cached: string | null) => {
+  try {
+    return cached ? JSON.parse(cached) : null
+  } catch (error) {
+    return null
+  }
+}
+const isValidData = (data: any) =>
+  data && typeof data === 'object' && Object.keys(data).length > 0
 export const getFromCache = async (key: CACHE_KEYS, fn: () => Promise<any>) => {
   const cached = localStorage.getItem(key)
 
-  if (cached) {
-    return JSON.parse(cached)
+  const parsed = getCachedData(cached)
+  if (isValidData(parsed)) {
+    return parsed
   }
-
   const data = await fn()
   localStorage.setItem(key, JSON.stringify(data))
   return data
